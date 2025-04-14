049cbe6a7fcc02db5b95576237d970d959632207
Nishant-devspark <mxnishant16@gmail.com>
2025-04-14 18:30:28 +0530

    Hotel

---
+++ script.js
@@ -0,0 +1,24 @@
+/* For clickable buttons or links */
+button, a, .clickable {
+    cursor: pointer;
+  }
+  
+  /* For text inputs or editable content */
+  input, textarea, [contenteditable="true"] {
+    cursor: text;
+  }
+  
+  /* For disabled elements */
+  .disabled {
+    cursor: not-allowed;
+  }
+  
+  /* For draggable items */
+  .draggable {
+    cursor: grab;
+  }
+  
+  .draggable:active {
+    cursor: grabbing;
+  }
+  
